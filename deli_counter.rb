5a2a5674882cd6480c39d8caa6fa49bc77ac8784
Burbage Stacey <rstacey06@gmail.com>
2019-11-07 15:07:56 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,7 +16,7 @@ def now_serving(deli)
   if deli.empty? 
     puts "There is nobody waiting to be served!"
   else
-    puts "Currently serving #{name}."
+    puts "Currently serving #{deli.first}."
 end
 end
 
